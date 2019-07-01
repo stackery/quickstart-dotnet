688630e82841395aac50c9e2863e9b83f2e5d853
Anna Spysz <anna.spysz@stackery.io>
2019-07-01 16:55:53 -0700

    WIP

--- src/postItem/Handler.cs
+++ src/postItem/Handler.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Collections.Generic;
 using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2;
 
 namespace StackeryFunction
 {
     public class Handler
     {
-        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
         public dynamic handler(dynamic eventTrigger)
         {
-            Console.WriteLine(eventTrigger);
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+            var tableName = Environment.GetEnvironmentVariable("TABLE_NAME");
+
+            var config = new DynamoDBContextConfig { Conversion = DynamoDBEntryConversion.V2 };
+            this.DDBContext = new DynamoDBContext(new AmazonDynamoDBClient(), config);
 
             return new {};
         }
