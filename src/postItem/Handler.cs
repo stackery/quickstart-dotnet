27258d66f806d22c5463f3308d654ccd0f4f06d8
Jun Marcel Fritz <jun.fritz@gmail.com>
2019-07-01 17:50:01 -0700

    add working getItems + postItem calls, event object handling WIP.

--- src/postItem/Handler.cs
+++ src/postItem/Handler.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 
 namespace StackeryFunction
 {
@@ -9,8 +12,27 @@ namespace StackeryFunction
         public dynamic handler(dynamic eventTrigger)
         {
             Console.WriteLine(eventTrigger);
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
 
-            return new {};
+            var tableName = Environment.GetEnvironmentVariable("TABLE_NAME");
+            Console.WriteLine(tableName);
+            var id = "1";
+            var content = "newItem";
+            // Console.WriteLine("Writing " + skmax + " items for partition key: " + ipk);
+            var request = new PutItemRequest()
+            {
+                TableName = tableName,
+                Item = new Dictionary<string, AttributeValue>()
+                {
+                    { "id", new AttributeValue{S = id } },
+                    { "content", new AttributeValue{S = content } }
+                }
+            };
+
+            var response = client.PutItemAsync(request).Result;
+
+            Console.WriteLine("Done!");
+            return response;
         }
     }
 }
